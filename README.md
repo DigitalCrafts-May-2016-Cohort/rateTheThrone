c577fce2e3f72e25239b387395a4cc5e6b695398
Tim Sanders <tsanders30004@gmail.com>
2016-06-13 14:10:46 -0400

    added API calls and README

---
+++ README.md
@@ -0,0 +1,35 @@
+#Rate the Throne
+
+##Overview of Project
+Rate the Throne is a crowdsourced public restroom rating app. Users can use Rate the Throne to view and contribute ratings of public restrooms in their surrounding area.
+
+
+##Technologies, Frameworks, and Programming Languages used
+-HTML5, CSS3
+-Bootstrap 3
+-JavaScript
+-Google Maps API
+-AngularJS
+-Animate CSS
+
+
+##Contributors
+-Cody Barber
+-Carolyn Daniel
+-DeeAnn Kendrick
+-Tim Sanders
+
+##Name of each team member and his/her individual contributions to the project
+-Cody Barber
+-Carolyn Daniel
+-DeeAnn Kendrick
+-Tim Sanders
+
+
+##Project Screenshots
+
+
+##3 contributions your team would like for others to add to the project
+
+
+##URL to live project
---
+++ js/app.js
@@ -0,0 +1,58 @@
+
+
+//put map up
+
+var map;
+var infowindow;
+
+function initMap() {
+     var atlanta = {lat: 33.748995, lng: -84.387982};
+     var element = document.getElementById('map');
+     var mapOptions = {
+          center: atlanta,
+          zoom: 15
+     };
+     map = new google.maps.Map(element, mapOptions);
+
+     var request = {
+          location: atlanta,
+          radius: 500,
+          query: "gas_station"
+     };
+
+     infowindow = new google.maps.InfoWindow();
+
+     var service = new google.maps.places.PlacesService(map);
+     service.textSearch(request, callback);
+
+}
+
+function callback(results, status) {
+     if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+               var place = results[i];
+               createMarker(results[i]);
+          }
+     }
+     console.log(results);
+}
+
+function createMarker(place) {
+     var placeLoc = place.geometry.location;
+     var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+     });
+
+
+     google.maps.event.addListener(marker, 'click', function() {
+          var windowContent =
+          '<div class="content"><p>' + place.name + '<br>' +
+          place.formatted_address + '<br>throne rating: ' + 'rating' +
+          '<br><a href="#">rate this throne</a></p></div>';
+
+          infowindow.setContent(windowContent);
+          infowindow.open(map, this);
+     });
+
+}
