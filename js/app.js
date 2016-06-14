d41d108bd16351f694784abd14fbdb9057d6f142
Cody Barber <cody.n.barber@gmail.com>
2016-06-14 18:57:32 -0400

    services

--- js/app.js
+++ js/app.js
@@ -1,82 +1,105 @@
 var app = angular.module('rateThroneApp', []);
 
-app.controller('MainController', function($scope) {
-  var atlanta = {lat: 33.748995, lng: -84.387982};
-  var element = document.getElementById('map');
-  var mapOptions = {
-    center: atlanta,
-    zoom: 15
-  };
-  map = new google.maps.Map(element, mapOptions);
-  infowindow = new google.maps.InfoWindow();
+var map;
+var infowindow;
+var atlanta = {lat: 33.748995, lng: -84.387982};
+var place;
 
-  var service = new google.maps.places.PlacesService(map);
-  // service.textSearch(request, callback);
-  service.nearbySearch({
-    location: atlanta,
-    radius: 850,
-    types: ['bar', 'store', 'restaurant']
-  }, callback);
+app.factory('googleMaps', function() {
+  return {
+    newMap: function(city) {
 
-  function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        var place = results[i];
-        createMarker(results[i]);
-      }
-    }
-    console.log(results);
-  }
+      var element = document.getElementById('map');
+      var mapOptions = {
+        center: atlanta,
+        zoom: 15
+      };
+      map = new google.maps.Map(element, mapOptions);
+      return map;
+    },
+    addMarker: function(result, map) {
+      infowindow = new google.maps.InfoWindow();
 
-  function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location,
-      icon: 'bathroomsymbolsmall.png'
-    });
-    function linkData(placeId) {
-      for (var i = 0; i < reviewData.length; i++) {
-        if (placeId === reviewData[i].place_id) {
-          console.log(reviewData[i]);
-          return reviewData[i];
+      var service = new google.maps.places.PlacesService(map);
+      // service.textSearch(request, callback);
+      service.nearbySearch({
+        location: atlanta,
+        radius: 850,
+        types: ['bar', 'store', 'restaurant']
+      }, callback);
+
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            place = results[i];
+            createMarker(results[i]);
+          }
         }
+        // console.log(results);
       }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location,
+          icon: 'bathroomsymbolsmall.png'
+        });
+        function linkData(placeId) {
+          for (var i = 0; i < reviewData.length; i++) {
+            if (placeId === reviewData[i].place_id) {
+              console.log(reviewData[i]);
+              return reviewData[i];
+            }
+          }
+        }
+        function ratingAverage(placeId) {
+          var someLocation = linkData(place.place_id);
+          var sum = 0;
+          if (someLocation.review.length === 0) {
+            return "No reviews yet!";
+          }
+          for (var i = 0; i < someLocation.review.length; i++) {
+            sum += someLocation.review[i].rating;
+          }
+          console.log(sum / someLocation.review.length);
+          return sum / someLocation.review.length + " stars";
+        }
+        google.maps.event.addListener(marker, 'click', function() {
+
+          var windowContent =
+          '<div class="content"><p>' + place.name + '<br>' +
+          place.vicinity + '<br>average throne rating: ' + ratingAverage(linkData(place.place_id)) +
+          '<br><a href="#" data-toggle="modal" data-target="#myModal">rate this throne</a></p>' + '<p class="rating"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>'+ '</div>';
+
+          infowindow.setContent(windowContent);
+          infowindow.open(map, this);
+        });
+      }
+
     }
-    function ratingAverage(placeId) {
-      var someLocation = linkData(place.place_id);
-      var sum = 0;
-      if (someLocation.review.length === 0) {
-        return "No reviews yet!";
-      }
-      for (var i = 0; i < someLocation.review.length; i++) {
-        sum += someLocation.review[i].rating;
-      }
-      console.log(sum / someLocation.review.length);
-      return sum / someLocation.review.length + " stars";
-    }
-
-
-
-    google.maps.event.addListener(marker, 'click', function($scope) {
-      $scope.place = place;
-      console.log(place);
-
-      var windowContent =
-      '<div class="content"><p>' + place.name + '<br>' +
-      place.vicinity + '<br>average throne rating: ' + ratingAverage(linkData(place.place_id)) +
-      '<br><a href="#" onclick="clickedLink(this)" data-place-id="' + place.place_id + '" data-toggle="modal" data-target="#myModal">view & rate this throne</a></p>' + '<p class="rating"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>'+ '</div>';
-
-      infowindow.setContent(windowContent);
-      infowindow.open(map, this);
-    });
-  }
-
-
-
+  };
 });
 
-function clickedLink(a) {
-  var placeId = $(a).data('place-id');
-  console.log(placeId);
-}
+
+
+app.controller('MainController', function($scope, googleMaps) {
+  var map = googleMaps.newMap(atlanta);
+  googleMaps.addMarker(place, map);
+  console.log(reviewData);
+});
+
+// app.controller('ReviewController', function($scope) {
+//   $scope.author = author;
+//   $scope.rating = rating;
+//   $scope.comment = comment;
+//
+//   $scope.addReview = function() {
+//     var reviewBeingAdded = {
+//       "author": $scope.author,
+//       "rating": $scope.rating,
+//       "comment": $scope.comment
+//     };
+//   };
+// });
