22280415004b68d312de8736f00fb5321308886d
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-06-14 09:50:32 -0400

    refactored to factory service

--- js/app.js
+++ js/app.js
@@ -1,111 +1,66 @@
-
 var app = angular.module('rateThroneApp', []);
 
-app.controller('MainController', function($scope) {
-  var atlanta = {lat: 33.748995, lng: -84.387982};
-  var element = document.getElementById('map');
-  var mapOptions = {
-    center: atlanta,
-    zoom: 15
-  };
-  map = new google.maps.Map(element, mapOptions);
-  infowindow = new google.maps.InfoWindow();
+var map;
+var infowindow;
+var atlanta = {lat: 33.748995, lng: -84.387982};
+var place;
 
-  var service = new google.maps.places.PlacesService(map);
-  // service.textSearch(request, callback);
-  service.nearbySearch({
-    location: atlanta,
-    radius: 850,
-    types: ['bar', 'store', 'restaurant']
-  }, callback);
+app.factory('googleMaps', function() {
+  return {
+    newMap: function(city) {
 
-  function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        var place = results[i];
-        createMarker(results[i]);
+      var element = document.getElementById('map');
+      var mapOptions = {
+        center: atlanta,
+        zoom: 15
+      };
+      map = new google.maps.Map(element, mapOptions);
+      return map;
+    },
+    addMarker: function(result, map) {
+      infowindow = new google.maps.InfoWindow();
+
+      var service = new google.maps.places.PlacesService(map);
+      // service.textSearch(request, callback);
+      service.nearbySearch({
+        location: atlanta,
+        radius: 850,
+        types: ['bar', 'store', 'restaurant']
+      }, callback);
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            place = results[i];
+            createMarker(results[i]);
+          }
+        }
+        console.log(results);
+      }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+          var windowContent =
+          '<div class="content"><p>' + place.name + '<br>' +
+          place.vicinity + '<br>throne rating: ' + 'rating' +
+          '<br><a href="#">rate this throne</a></p>' + '<p class="rating"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>'+ '</div>';
+
+          infowindow.setContent(windowContent);
+          infowindow.open(map, this);
+        });
       }
     }
-    console.log(results);
-  }
-
-  function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-      var windowContent =
-      '<div class="content"><p>' + place.name + '<br>' +
-      place.vicinity + '<br>throne rating: ' + 'rating' +
-      '<br><a href="#">rate this throne</a></p>' + '<p class="rating"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>'+ '</div>';
-
-      infowindow.setContent(windowContent);
-      infowindow.open(map, this);
-    });
-
-  }
+  };
 });
-// //put map up
-//
-// var map;
-// var infowindow;
-//
-// function initMap() {
-//      var atlanta = {lat: 33.748995, lng: -84.387982};
-//      var element = document.getElementById('map');
-//      var mapOptions = {
-//           center: atlanta,
-//           zoom: 15
-//      };
-//      map = new google.maps.Map(element, mapOptions);
-//
-//      // var request = {
-//      //      location: atlanta,
-//      //      radius: 500,
-//      //      query: "gas_station"
-//      // };
-//
-//      infowindow = new google.maps.InfoWindow();
-//
-//      var service = new google.maps.places.PlacesService(map);
-//      // service.textSearch(request, callback);
-//      service.nearbySearch({
-//        location: atlanta,
-//        radius: 850,
-//        types: ['bar', 'store', 'restaurant']
-//      }, callback);
-//
-//
-// }
-//
-// function callback(results, status) {
-//      if (status === google.maps.places.PlacesServiceStatus.OK) {
-//           for (var i = 0; i < results.length; i++) {
-//                var place = results[i];
-//                createMarker(results[i]);
-//           }
-//      }
-//      console.log(results);
-// }
-//
-// function createMarker(place) {
-//      var placeLoc = place.geometry.location;
-//      var marker = new google.maps.Marker({
-//           map: map,
-//           position: place.geometry.location
-//      });
-//
-//
-//      google.maps.event.addListener(marker, 'click', function() {
-//           var windowContent =
-//           '<div class="content"><p>' + place.name + '<br>' +
-//           place.formatted_address + '<br>throne rating: ' + 'rating' +
-//           '<br><a href="#">rate this throne</a></p>' + '<p class="rating"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>'+ '</div>';
-//
-//           infowindow.setContent(windowContent);
-//           infowindow.open(map, this);
-//      });
-//
-// }
+
+
+
+app.controller('MainController', function($scope, googleMaps) {
+  var map = googleMaps.newMap(atlanta);
+  googleMaps.addMarker(place, map);
+});
