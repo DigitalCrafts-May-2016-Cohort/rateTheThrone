dd42e042ddca2f76904423335c8d08050609cee3
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-06-17 09:14:42 -0400

    commented code

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 var app = angular.module('rateThroneApp', ['ui.bootstrap']);
 
 var map;
-var infowindow;
 var atlanta = {lat: 33.8486730, lng: -84.3733130};
 var place;
 
+//modal controller
 app.controller('ModalController', function($scope, place, $uibModalInstance) {
   $scope.place = place;
-  $scope.location = linkData(place.place_id);
-  $scope.averageRating = ratingAverage(linkData(place.place_id));
+  $scope.location = linkDataModal(place.place_id);
+  $scope.averageRating = ratingAverageModal(linkDataModal(place.place_id));
   $scope.modalReviewData = $scope.location.review;
   $scope.publicOrNot = $scope.location.public;
 
+  //add a new review function
   $scope.addNewReview = function(author, rating, comment) {
     $scope.newReview = {
       "author": author,
@@ -23,15 +24,18 @@ app.controller('ModalController', function($scope, place, $uibModalInstance) {
     document.getElementById('submit-review').style.visibility='hidden';
   };
 
-  function linkData(placeId) {
+  //function to link data from Google Maps to our data for modal
+  function linkDataModal(placeId) {
     for (var i = 0; i < reviewData.length; i++) {
       if (placeId === reviewData[i].place_id) {
         return reviewData[i];
       }
     }
   }
-  function ratingAverage(placeId) {
-    var someLocation = linkData(place.place_id);
+
+  //function to find averge rating for modal
+  function ratingAverageModal(placeId) {
+    var someLocation = linkDataModal(place.place_id);
     var sum = 0;
     if (someLocation.review.length === 0) {
       return "No reviews yet!";
@@ -41,6 +45,7 @@ app.controller('ModalController', function($scope, place, $uibModalInstance) {
     }
     var avg = Math.ceil(sum / someLocation.review.length);
     console.log ("the avg is:" + avg);
+    //add stars instead of text
     if (avg === 5) {
       return "★★★★★";
     } else if (avg === 4) {
@@ -56,12 +61,14 @@ app.controller('ModalController', function($scope, place, $uibModalInstance) {
     }
   }
 
+  //close out of modal
   $scope.cancel = function () {
     $uibModalInstance.dismiss('cancel');
   };
 
 });
 
+//modal service
 app.factory('modal', function($uibModal){
   return {
     openModal: function(place, size) {
@@ -80,14 +87,17 @@ app.factory('modal', function($uibModal){
   };
 });
 
+
+//google maps service
 app.factory('googleMaps', function($uibModal, modal) {
   return {
+    //build map
     newMap: function(city) {
-
       var element = document.getElementById('map');
       var mapOptions = {
         center: atlanta,
         zoom: 17,
+        //change map colors
         styles: [
           {
             "featureType": "landscape",
@@ -196,18 +206,17 @@ app.factory('googleMaps', function($uibModal, modal) {
       map = new google.maps.Map(element, mapOptions);
       return map;
     },
+    //add markers to map
     addMarker: function(result, map, $scope) {
-      infowindow = new google.maps.InfoWindow();
-
       var service = new google.maps.places.PlacesService(map);
-      // service.textSearch(request, callback);
+      //get locations based off of type - bar, store, restaurant
       service.nearbySearch({
         location: atlanta,
         radius: 850,
         types: ['bar', 'store', 'restaurant']
       }, callback);
 
-
+      //callback function for marker results
       function callback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
           for (var i = 0; i < results.length; i++) {
@@ -217,12 +226,13 @@ app.factory('googleMaps', function($uibModal, modal) {
         }
       }
 
+      //function to create marker - using different symbols
       function createMarker(place) {
         var placeLoc = place.geometry.location;
         var theIcon = 'bathroomsymbolsmall.png';
         var theRating = ratingAverage(place.place_id);
         var publicRestroom = linkData(place.place_id).public;
-
+        //different icons
         if (publicRestroom === false) {
           theIcon = 'images/noGo.png';
         } else {
@@ -232,51 +242,53 @@ app.factory('googleMaps', function($uibModal, modal) {
             theIcon = 'images/green.png';
           } else if (theRating < 2) {
             theIcon = 'images/red.png';
-          }}
-
-
-          var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location,
-            icon: theIcon
-            // 'bathroomsymbolsmall.png'
-          });
-          function linkData(placeId) {
-            for (var i = 0; i < reviewData.length; i++) {
-              if (placeId === reviewData[i].place_id) {
-                console.log(reviewData[i]);
-                return reviewData[i];
-              }
-            }
           }
-          function ratingAverage(placeId) {
-            var someLocation = linkData(place.place_id);
-            var sum = 0;
-            if (someLocation.review.length === 0) {
-              return "No reviews yet!";
-            }
-            for (var i = 0; i < someLocation.review.length; i++) {
-              sum += someLocation.review[i].rating;
-            }
-            console.log(sum / someLocation.review.length);
-            return sum / someLocation.review.length;
-          }
-          // setTimeout(function() {
-          //   $scope.blah = 'NOT HELLO';
-          //   $scope.$apply();
-          // }, 1000);
-          google.maps.event.addListener(marker, 'click', function() {
-            modal.openModal(place, 'lg');
-          });
         }
 
+        //plot markers
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location,
+          icon: theIcon
+        });
+
+        //link google maps data with our data
+        function linkData(placeId) {
+          for (var i = 0; i < reviewData.length; i++) {
+            if (placeId === reviewData[i].place_id) {
+              console.log(reviewData[i]);
+              return reviewData[i];
+            }
+          }
+        }
+
+        //calculate average rating
+        function ratingAverage(placeId) {
+          var someLocation = linkData(place.place_id);
+          var sum = 0;
+          if (someLocation.review.length === 0) {
+            return "No reviews yet!";
+          }
+          for (var i = 0; i < someLocation.review.length; i++) {
+            sum += someLocation.review[i].rating;
+          }
+          console.log(sum / someLocation.review.length);
+          return sum / someLocation.review.length;
+        }
+
+        //click to open modal
+        google.maps.event.addListener(marker, 'click', function() {
+          modal.openModal(place, 'lg');
+        });
       }
-    };
-  });
+    }
+  };
+});
 
 
-
-  app.controller('MainController', function($scope, googleMaps, $uibModal, modal) {
-    var map = googleMaps.newMap(atlanta);
-    googleMaps.addMarker(place, map, $scope);
-  });
+//main controller
+app.controller('MainController', function($scope, googleMaps, $uibModal, modal) {
+  //add map
+  var map = googleMaps.newMap(atlanta);
+  googleMaps.addMarker(place, map, $scope);
+});
